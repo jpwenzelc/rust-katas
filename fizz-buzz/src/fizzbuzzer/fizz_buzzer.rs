d0648c38397b6cb4749f7062094a2cca3db3ce45
Jose Pablo Wenzel <devwenzel@gmail.com>
2022-07-07 12:02:53 +0100

    typo with return

--- fizz-buzz/src/fizzbuzzer/fizz_buzzer.rs
+++ fizz-buzz/src/fizzbuzzer/fizz_buzzer.rs
@@ -20,7 +20,7 @@ mod tests {
     #[case(2)]
     #[case(4)]
     #[case(7)]
-    fn that_retuns_a_number_if_not_divisible_by_3_or_5(#[case] input: i32) {
+    fn that_returns_a_number_if_not_divisible_by_3_or_5(#[case] input: i32) {
         let result = fizz_buzzer(input);
         assert_eq!(result, input.to_string());
     }
@@ -30,7 +30,7 @@ mod tests {
     #[case(6)]
     #[case(9)]
     #[case(12)]
-    fn that_retuns_fizz_if_is_divisible_by_3(#[case] input: i32) {
+    fn that_returns_fizz_if_is_divisible_by_3(#[case] input: i32) {
         let result = fizz_buzzer(input);
         assert_eq!(result, String::from("fizz"));
     }
@@ -40,7 +40,7 @@ mod tests {
     #[case(10)]
     #[case(20)]
     #[case(25)]
-    fn that_retuns_buzz_if_is_divisible_by_5(#[case] input: i32) {
+    fn that_returns_buzz_if_is_divisible_by_5(#[case] input: i32) {
         let result = fizz_buzzer(input);
         assert_eq!(result, String::from("buzz"));
     }
@@ -49,7 +49,7 @@ mod tests {
     #[case(15)]
     #[case(30)]
     #[case(45)]
-    fn that_retuns_fizzbuzz_if_is_divisible_by_3_and_5(#[case] input: i32) {
+    fn that_returns_fizzbuzz_if_is_divisible_by_3_and_5(#[case] input: i32) {
         let result = fizz_buzzer(input);
         assert_eq!(result, String::from("fizzbuzz"));
     }
